0d88fdcd5bfbb665a1b7d7ff541ac4e14cddf778
Nguyễn Lê Hải <nguyenlehai@Nguyens-MacBook-Pro.local>
2022-11-27 23:16:21 +0700

    update

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -7,51 +7,53 @@ export default function Category() {
       <div className="container">
         <div className='row'>
           <div class="col-sm-12">
-          <div class="categories">
-            <div class="categories__title">
-              <h3>Games categories</h3>
-            </div>
-            <div class="row">
-              <div class="col text-left">
-                <div class="categories__block adventure d-flex justify-content-lg-start align-items-baseline">
-                  <div class="categories__block img-adventure">
-                    <img src={require('../../assets/images/icon-category/adventure.svg').default} />
-                  </div>
-                  <div class="categories__block text">
-                    <p>Adventures</p>
-                  </div>
-                </div>
-                <div class="categories__block fantasiya d-flex justify-content-lg-start align-items-baseline">
-                  <div class="categories__block img-fantasiya">
-                    <img src={require('../../assets/images/icon-category/adventure.svg').default} />
-                  </div>
-                  <div class="categories__block text">
-                    <p>Fantasiya</p>
-                  </div>
-                </div>
+            <div class="categories">
+              <div class="categories__title">
+                <h3>Games categories</h3>
               </div>
-              <div class="col text-left">
-                <div class="categories__block arcade d-flex justify-content-lg-start align-items-baseline">
-                  <div class="categories__block img-arcade ">
-                    <img src={require('../../assets/images/icon-category/adventure.svg').default} />
+              <div class="row">
+                <div class="col d-grid
+    justify-content-start;
+    ">
+                  <div class="categories__block adventure d-flex justify-content-lg-start align-items-baseline">
+                    <div class="categories__block img-adventure">
+                      <img src={require('../../assets/images/icon-category/adventure.svg').default} />
+                    </div>
+                    <div class="categories__block text">
+                      <p>Adventures</p>
+                    </div>
                   </div>
-                  <div class="categories__block text">
-                    <p>Arcade-an</p>
+                  <div class="categories__block fantasiya d-flex justify-content-lg-start align-items-baseline">
+                    <div class="categories__block img-fantasiya">
+                      <img src={require('../../assets/images/icon-category/adventure.svg').default} />
+                    </div>
+                    <div class="categories__block text">
+                      <p>Fantasiya</p>
+                    </div>
                   </div>
                 </div>
-
-                <div class="categories__block archi d-flex justify-content-lg-start align-items-baseline">
-                  <div class="categories__block img-archi">
-                    <img src={require('../../assets/images/icon-category/adventure.svg').default} />
+                <div class="col text-left">
+                  <div class="categories__block arcade d-flex justify-content-lg-start align-items-baseline">
+                    <div class="categories__block img-arcade ">
+                      <img src={require('../../assets/images/icon-category/adventure.svg').default} />
+                    </div>
+                    <div class="categories__block text">
+                      <p>Arcade-an</p>
+                    </div>
                   </div>
-                  <div class="categories__block text">
-                    <p>Architectures</p>
+
+                  <div class="categories__block archi d-flex justify-content-lg-start align-items-baseline">
+                    <div class="categories__block img-archi">
+                      <img src={require('../../assets/images/icon-category/adventure.svg').default} />
+                    </div>
+                    <div class="categories__block text">
+                      <p>Architectures</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
         </div>
       </div>
     </section>
